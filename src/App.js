906d01838d07acff40a546360a0e4be6a819a5d3
SHIVAMSINGH4 <singh4248212@gmail.com>
2021-12-18 06:55:41 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     test shivam test
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
+
+      tets
     </div>
   );
 }
