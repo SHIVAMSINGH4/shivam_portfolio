34343073001459645a96f43d2858ee8cf621b9d8
SHIVAMSINGH4 <singh4248212@gmail.com>
2021-12-24 12:52:08 +0530

    home edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,29 @@
+import '../css/home.css'
+
 function Home() {
+
     return (
         <>
-            Home
+            <div className="container-fluid" style={{
+                backgroundColor: '#212529',
+                height: '800px'
+            }}>
+                <div className="row">
+                    <div className="col-md-2 pt-5 mt-5 ">
+
+                    </div>
+                    <div className="col-md-5 pt-5 mt-5 ">
+                        <h2 style={{ color: 'white' }}>Hi, I'm Shivam Singh.</h2>
+                    </div>
+                    <div className="col-md-5 pt-5 mt-5" >
+                        <img src={`${process.env.PUBLIC_URL}/img/myImg.jpg`} alt="Avatar" />
+                    </div>
+                </div>
+            </div>
+
+
         </>
+
     )
 }
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import Home from './components/Home';
 import About from './components/about'
 import Contact from './components/contact'
 import Project from './components/projects'
+import Header from './components/header'
 ReactDOM.render(
   <React.StrictMode>
-
+    <Header />
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
